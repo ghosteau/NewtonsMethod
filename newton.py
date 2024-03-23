7e443265aeeae583466d69e39304fbb827a1211c
Manny <152332440+ghosteau@users.noreply.github.com>
2024-03-23 18:20:26 -0400

    Update newton.py

--- newton.py
+++ newton.py
@@ -1,28 +1,30 @@
 import sympy
 from sympy import *
 from sympy.calculus.util import continuous_domain
+
 def init_symbol(input_variable):
     if input_variable != str(input_variable):
         raise Exception("Must input string to initialize symbol")
-
     return symbols(input_variable)
 
 def init_function(input_function):
     if input_function != str(input_function):
         raise Exception("Must input string to initialize function")
-
     expression = sympify(input_function)
     return expression
 
 def domain(input_function):
+    x = symbols("x")
     domain = continuous_domain(input_function, x, sympy.Reals)
     return domain
 
 def find_undefined(input_function):
+    x = symbols("x")
     domain = continuous_domain(input_function, x, sympy.Reals)
     return Complement(sympy.Reals, domain)
 
 def newton_method(input_function, x0, iterations):
+    x = symbols("x")
     function_prime = input_function.diff(x)
 
     if x0 not in domain(input_function):
@@ -40,9 +42,8 @@ def newton_method(input_function, x0, iterations):
 
     return new_x
 
-initialize_var = input("Enter what variable you are going to use for your function (must be a string/character): ")
-init_symbol(initialize_var)
-new_func = input("Input your function -- be aware of how to write different operations: ")
+init_symbol("x")
+new_func = input("Input your function in terms of x -- be aware of how to write different operations: ")
 new_func = init_function(new_func)
 init_guess = input("What value would you like the algorithm to start with? (this is x-sub-zero): ")
 init_guess = int(init_guess)
